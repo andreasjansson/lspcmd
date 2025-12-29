5f08ac377b05bbf4e1e2866f0c97a3c6b664bc17
Andreas Jansson <ajansson@cloudflare.com>
2025-12-29 22:29:39 +0100

    Test workspaceFolders capability

--- lspcmd/lsp/capabilities.py
+++ lspcmd/lsp/capabilities.py
@@ -4,16 +4,6 @@ from typing import Any
 def get_client_capabilities() -> dict[str, Any]:
     return {
         "workspace": {
-            "applyEdit": True,
-            "workspaceEdit": {
-                "documentChanges": True,
-                "resourceOperations": ["create", "rename", "delete"],
-            },
-            "symbol": {
-                "dynamicRegistration": False,
-                "symbolKind": {"valueSet": list(range(1, 27))},
-            },
-            "executeCommand": {"dynamicRegistration": False},
             "workspaceFolders": True,
         },
     }
