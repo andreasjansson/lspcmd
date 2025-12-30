a79974754e4b96fabef8280d498602343143aca8
Andreas Jansson <ajansson@cloudflare.com>
2025-12-30 12:39:47 +0100

    feat: add find-implementations handler

--- lspcmd/daemon/server.py
+++ lspcmd/daemon/server.py
@@ -277,6 +277,9 @@ class DaemonServer:
     async def _handle_find_declaration(self, params: dict) -> list[dict]:
         return await self._handle_location_request(params, "textDocument/declaration")
 
+    async def _handle_find_implementations(self, params: dict) -> list[dict]:
+        return await self._handle_location_request(params, "textDocument/implementation")
+
     async def _handle_find_references(self, params: dict) -> list[dict]:
         workspace, doc, path = await self._get_workspace_and_document(params)
         line, column = self._parse_position(params)
