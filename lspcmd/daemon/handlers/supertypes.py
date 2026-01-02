33a61785bae08908f726b2eefa6abb1d6dc29f59
Andreas Jansson <ajansson@cloudflare.com>
2026-01-02 14:40:20 +0100

    Add handlers/supertypes.py

---
+++ lspcmd/daemon/handlers/supertypes.py
@@ -0,0 +1,54 @@
+"""Handler for supertypes command."""
+
+from ..rpc import SupertypesParams, SupertypesResult, LocationInfo
+from ...lsp.protocol import LSPResponseError, LSPMethodNotSupported
+from .base import HandlerContext
+
+
+async def handle_supertypes(
+    ctx: HandlerContext, params: SupertypesParams
+) -> SupertypesResult:
+    workspace, doc, path = await ctx.get_workspace_and_document({
+        "path": params.path,
+        "workspace_root": params.workspace_root,
+    })
+    line, column = ctx.parse_position({"line": params.line, "column": params.column})
+    context = params.context
+
+    await workspace.client.wait_for_service_ready()
+
+    try:
+        prepare_result = await workspace.client.send_request(
+            "textDocument/prepareTypeHierarchy",
+            {
+                "textDocument": {"uri": doc.uri},
+                "position": {"line": line, "character": column},
+            },
+        )
+    except LSPResponseError as e:
+        if e.is_method_not_found():
+            raise LSPMethodNotSupported(
+                "textDocument/prepareTypeHierarchy", workspace.server_config.name
+            )
+        raise
+
+    if not prepare_result:
+        return SupertypesResult(locations=[])
+
+    item = prepare_result[0]
+
+    try:
+        result = await workspace.client.send_request(
+            "typeHierarchy/supertypes", {"item": item}
+        )
+    except LSPResponseError as e:
+        if e.is_method_not_found():
+            raise LSPMethodNotSupported(
+                "typeHierarchy/supertypes", workspace.server_config.name
+            )
+        raise
+
+    locations = ctx.format_type_hierarchy_items(result, workspace.root, context)
+    return SupertypesResult(
+        locations=[LocationInfo(**loc) for loc in locations]
+    )
