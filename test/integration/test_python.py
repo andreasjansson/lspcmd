4cfdfc9f0745995db346e1acc55cde514a7d2bf7
Andreas Jansson <ajansson@cloudflare.com>
2026-01-04 01:22:10 +0100

    test/integration/python: update test_grep_single_file expectations for new line numbers

--- test/integration/test_python.py
+++ test/integration/test_python.py
@@ -70,49 +70,49 @@ main.py:17 [Variable] value in save
 main.py:21 [Method] load in StorageProtocol
 main.py:21 [Variable] key in load
 main.py:27 [Class] User
-main.py:35 [Variable] name in User
-main.py:36 [Variable] email in User
-main.py:37 [Variable] age in User
-main.py:39 [Method] is_adult in User
-main.py:43 [Method] display_name in User
-main.py:48 [Class] MemoryStorage
-main.py:51 [Method] __init__ in MemoryStorage
-main.py:54 [Method] save in MemoryStorage
-main.py:54 [Variable] key in save
-main.py:54 [Variable] value in save
-main.py:57 [Method] load in MemoryStorage
-main.py:57 [Variable] key in load
-main.py:52 [Variable] _data in MemoryStorage
-main.py:61 [Class] FileStorage
-main.py:64 [Method] __init__ in FileStorage
-main.py:64 [Variable] base_path in __init__
-main.py:67 [Method] save in FileStorage
-main.py:67 [Variable] key in save
-main.py:67 [Variable] value in save
-main.py:68 [Variable] path in save
-main.py:69 [Variable] f in save
-main.py:72 [Method] load in FileStorage
-main.py:72 [Variable] key in load
-main.py:73 [Variable] path in load
-main.py:75 [Variable] f in load
-main.py:65 [Variable] _base_path in FileStorage
-main.py:80 [Class] UserRepository
-main.py:86 [Method] __init__ in UserRepository
-main.py:89 [Method] add_user in UserRepository
-main.py:89 [Variable] user in add_user
-main.py:93 [Method] get_user in UserRepository
-main.py:93 [Variable] email in get_user
-main.py:97 [Method] delete_user in UserRepository
-main.py:97 [Variable] email in delete_user
-main.py:104 [Method] list_users in UserRepository
-main.py:108 [Method] count_users in UserRepository
-main.py:87 [Variable] _users in UserRepository
-main.py:113 [Function] create_sample_user
-main.py:118 [Function] process_users
-main.py:118 [Variable] repo in process_users
-main.py:127 [Function] main
-main.py:129 [Variable] repo in main
-main.py:130 [Variable] user in main
+main.py:36 [Variable] name in User
+main.py:37 [Variable] email in User
+main.py:38 [Variable] age in User
+main.py:40 [Method] is_adult in User
+main.py:44 [Method] display_name in User
+main.py:49 [Class] MemoryStorage
+main.py:52 [Method] __init__ in MemoryStorage
+main.py:55 [Method] save in MemoryStorage
+main.py:55 [Variable] key in save
+main.py:55 [Variable] value in save
+main.py:58 [Method] load in MemoryStorage
+main.py:58 [Variable] key in load
+main.py:53 [Variable] _data in MemoryStorage
+main.py:62 [Class] FileStorage
+main.py:65 [Method] __init__ in FileStorage
+main.py:65 [Variable] base_path in __init__
+main.py:68 [Method] save in FileStorage
+main.py:68 [Variable] key in save
+main.py:68 [Variable] value in save
+main.py:69 [Variable] path in save
+main.py:70 [Variable] f in save
+main.py:73 [Method] load in FileStorage
+main.py:73 [Variable] key in load
+main.py:74 [Variable] path in load
+main.py:76 [Variable] f in load
+main.py:66 [Variable] _base_path in FileStorage
+main.py:81 [Class] UserRepository
+main.py:87 [Method] __init__ in UserRepository
+main.py:90 [Method] add_user in UserRepository
+main.py:90 [Variable] user in add_user
+main.py:94 [Method] get_user in UserRepository
+main.py:94 [Variable] email in get_user
+main.py:98 [Method] delete_user in UserRepository
+main.py:98 [Variable] email in delete_user
+main.py:105 [Method] list_users in UserRepository
+main.py:109 [Method] count_users in UserRepository
+main.py:88 [Variable] _users in UserRepository
+main.py:114 [Function] create_sample_user
+main.py:119 [Function] process_users
+main.py:119 [Variable] repo in process_users
+main.py:128 [Function] main
+main.py:130 [Variable] repo in main
+main.py:131 [Variable] user in main
 main.py:138 [Variable] found in main"""
         )
 
