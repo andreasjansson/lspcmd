dc9be2a6da1853ea8bbe54cbd035861757c1e2ba
Andreas Jansson <ajansson@cloudflare.com>
2026-01-12 15:12:29 +0100

    chore: remove debug logging from Reporter::report

--- crates/leta-daemon/src/profiling.rs
+++ crates/leta-daemon/src/profiling.rs
@@ -22,14 +22,7 @@ impl CollectingReporter {
 
 impl Reporter for CollectingReporter {
     fn report(&mut self, spans: Vec<SpanRecord>) {
-        let count = spans.len();
-        let mut all = self.spans.lock().unwrap();
-        all.extend(spans);
-        tracing::info!(
-            "Reporter::report: received {} spans, total now {}",
-            count,
-            all.len()
-        );
+        self.spans.lock().unwrap().extend(spans);
     }
 }
 
